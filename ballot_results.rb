24319391ceaaddf592ac4b8d332b48ea5d620bc3
philiptang42 <philiptang42@gmail.com>
2015-12-02 19:04:14 -0500

    set initial data

---
+++ ballot_results.rb
@@ -0,0 +1,24 @@
+vote_results = []
+4.times do |precinct_index|
+  vote_results[precinct_index] = []
+  3.times do |candidate_index|
+    vote_results[precinct_index][candidate_index] = rand(20..500)
+  end
+end
+
+candidates = [
+  'Jim Bob',
+  'Sally Jane',
+  'Billy Joe'
+]
+
+#list out vote results
+vote_results.each_with_index do |precinct_results, precinct_index|
+  puts "**In precinct #{precinct_index + 1}:**"
+
+  candidates.each_with_index do |candidate, candidate_index|
+    puts "* #{candidate} got #{vote_results[precinct_index][candidate_index]} votes"
+  end
+
+  puts ""
+end
